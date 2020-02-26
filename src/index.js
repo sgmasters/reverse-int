2248fb885b073872eff0bf4736a013cf88a27506
Zhenya Rusak <unsgmasters@gmail.com>
2020-02-26 22:07:34 +0300

    feat: implemented reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let string = n + '';
+    let array = string.split("");
+    let reversedArray = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        reversedArray.push(array[i]);
+    }
+    let reversedString = "";
+    reversedArray.forEach((item) => {
+       reversedString+=item;
+    });
+    return parseInt(reversedString);
 }
